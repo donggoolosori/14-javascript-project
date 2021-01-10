a94a909b46a804bf65b72f35005fc489c713de9b
donggoolosori <kdj6551@naver.com>
2021-01-10 19:46:40 +0900

    Finish project 4

--- 4-navbar/app.js
+++ 4-navbar/app.js
@@ -3,3 +3,9 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+const links = document.querySelector(".links");
+const toggleBtn = document.querySelector(".nav-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
